119f67a930e9f97b5ff4c6879e634687277cd00a
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-10-23 22:04:09 +0100

    View Course, CoursesController

---
+++ MktAcademy/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MktAcademy.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Area { get; set; }
+
+        public string Remarks { get; set; }
+    }
+}
